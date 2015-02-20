181dd2eaad4185dac39dd38d9565876e1ed1c124
Tianon Gravi <admwiggin@gmail.com>
2015-02-20 00:25:05 -0700

    Add generate-stackbrew-library.sh

---
+++ generate-stackbrew-library.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
+
+url='git://github.com/docker-library/ghost'
+
+echo '# maintainer: InfoSiftr <github@infosiftr.com> (@infosiftr)'
+
+commit="$(git log -1 --format='format:%H' -- .)"
+fullVersion="$(grep -m1 'ENV GHOST_VERSION ' Dockerfile | cut -d' ' -f3)"
+
+versionAliases=()
+while [ "${fullVersion%.*}" != "$fullVersion" ]; do
+	versionAliases+=( $fullVersion )
+	fullVersion="${fullVersion%.*}"
+done
+versionAliases+=( $fullVersion latest )
+
+echo
+for va in "${versionAliases[@]}"; do
+	echo "$va: ${url}@${commit}"
+done

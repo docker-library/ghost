199453314fa15aef157d9e7f8ecfdbba16d8d073
Tianon Gravi <admwiggin@gmail.com>
2015-10-21 13:46:44 -0700

    Switch update.sh to use "git ls-remote" instead of hitting the GitHub API

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-current="$(curl -sSL 'https://api.github.com/repos/TryGhost/Ghost/releases/latest' | awk -F '": "|"' '$2 == "tag_name" { print $3; exit }')"
+current="$(git ls-remote --tags https://github.com/TryGhost/Ghost | cut -d$'\t' -f2 | grep -E '^refs/tags/[0-9]+\.[0-9]+' | cut -d/ -f3 | sort -rV | head -1)"
 
 set -x
 sed -ri 's/^(ENV GHOST_VERSION) .*/\1 '"$current"'/' Dockerfile

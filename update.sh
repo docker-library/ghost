0663fa9f41ed84f9f164e6a254a7078dfc3b4689
Tianon Gravi <admwiggin@gmail.com>
2016-06-06 11:57:57 -0700

    Ignore "beta" releases (for now?)

--- update.sh
+++ update.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 set -e
 
-current="$(git ls-remote --tags https://github.com/TryGhost/Ghost | cut -d$'\t' -f2 | grep -E '^refs/tags/[0-9]+\.[0-9]+' | cut -d/ -f3 | sort -rV | head -1)"
+current="$(
+	git ls-remote --tags https://github.com/TryGhost/Ghost.git \
+		| cut -d$'\t' -f2 \
+		| grep -E '^refs/tags/[0-9]+\.[0-9]+' \
+		| cut -d/ -f3 \
+		| grep -vE 'beta' \
+		| sort -rV \
+		| head -1
+)"
 
 set -x
 sed -ri 's/^(ENV GHOST_VERSION) .*/\1 '"$current"'/' Dockerfile

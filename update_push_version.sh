fdf6f9ff49f61511c2c52995a7066de5aa40d688
Pascal Andy <pascal@firepress.org>
2019-05-13 21:16:11 -0400

    Add update_push_version.sh in master

---
+++ update_push_version.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# based on https://github.com/mmornati/docker-ghostblog/blob/master/update_push_version.sh
+
+# A better class of script
+set -o errexit          # Exit on most errors (see the manual)
+set -o errtrace         # Make sure any error trap is inherited
+set -o nounset          # Disallow expansion of unset variables
+set -o pipefail         # Use last non-zero exit code in a pipeline
+#set -o xtrace          # Trace the execution of the script (debug)
+
+VERSION=$1 && \
+sed -i '' "s/^ARG GHOST_VERSION=.*$/ARG GHOST_VERSION=\"$VERSION\"/" Dockerfile && \
+git add . && \
+git commit -m "Ghost updated to $VERSION version" && \
+git tag -a $VERSION -m "Ghost updated to $VERSION version" && \
+git push --tags

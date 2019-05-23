755dbcc0da3fd39dc186bb3e133acb220e97c7e3
Pascal Andy <pascal@firepress.org>
2019-05-23 16:23:33 -0400

    Ghost updated to 2.22.3 version

--- run_version.sh
+++ run_version.sh
@@ -15,8 +15,7 @@ VERSION=$1 && \
 sed -i '' "s/^ARG GHOST_VERSION=.*$/ARG GHOST_VERSION=\"$VERSION\"/" Dockerfile && \
 git add . && \
 git commit -m "Ghost updated to $VERSION version" && \
-git tag -a $VERSION;
-
+git tag -a $VERSION -m "$VERSION";
 
 ##################################################
 # PUSH RELEASE

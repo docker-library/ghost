62c2727a8eba22918e91cd30bb69c2bfe9782de1
Pascal Andy <pascal@firepress.org>
2019-06-04 10:09:13 -0400

    RM tag comment
    Signed-off-by: Pascal Andy <pascal@firepress.org>

--- run_version.sh
+++ run_version.sh
@@ -15,7 +15,7 @@ VERSION=$1 && \
 sed -i '' "s/^ARG GHOST_VERSION=.*$/ARG GHOST_VERSION=\"$VERSION\"/" Dockerfile && \
 git add . && \
 git commit -m "Ghost updated to $VERSION version" && \
-git tag -a $VERSION -m "Ghost updated to $VERSION version" && \
+git tag $VERSION && \
 git push --tags;
 
 ##################################################

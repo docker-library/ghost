22a9e8be28f6eaf2d1187fe2ad89e487b09c6f19
Pascal Andy <pascal@firepress.org>
2019-06-16 21:34:52 -0400

    remove comment
    Signed-off-by: Pascal Andy <pascal@firepress.org>

--- runlint.sh
+++ runlint.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-# based on https://github.com/mmornati/docker-ghostblog/blob/master/update_push_version.sh
 
 # A better class of script
 set -o errexit          # Exit on most errors (see the manual)
@@ -8,7 +7,7 @@ set -o nounset          # Disallow expansion of unset variables
 set -o pipefail         # Use last non-zero exit code in a pipeline
 #set -o xtrace          # Trace the execution of the script (debug)
 
-docker run --rm hadolint/hadolint:v1.16.3-4-gc7f877d hadolint --version && echo;
+#docker run --rm hadolint/hadolint:v1.16.3-4-gc7f877d hadolint --version && echo;
 
 docker run --rm -i hadolint/hadolint:v1.16.3-4-gc7f877d hadolint \
   --ignore DL3000 \

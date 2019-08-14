47e5579e386c1b099b22db5876ca4201d784d3ea
Pascal Andy <pascal@firepress.org>
2019-08-14 17:52:15 -0400

    better local management
    Signed-off-by: Pascal Andy <pascal@firepress.org>

--- runrelease.sh
+++ runrelease.sh
@@ -1,18 +1,20 @@
 # Requires that we already and a branch tagged.
 # Usually via Tower
 
-# This is specific to my local set up
+# load secrets
 source .env .
 
 # vars
 #GITHUB_TOKEN="3122133122133211233211233211233211322313123"
 #local_repo="$Users/.../docker-stack-this"
-user="firepress-org"
-git_repo="ghostfire"
 
 # Find the latest tag
 tag_version="$(git tag --sort=-creatordate | head -n1)" && \
 echo ${tag_version} && \
+
+user="firepress-org"
+git_repo="ghostfire"
+GOPATH=$(go env GOPATH)
 
 # Requires https://github.com/aktau/github-release
 $GOPATH/bin/github-release release \
@@ -21,3 +23,7 @@ $GOPATH/bin/github-release release \
   --tag ${tag_version} \
   --name ${tag_version} \
   --description "Refer to [CHANGELOG.md](https://github.com/firepress-org/ghostfire/blob/master/CHANGELOG.md) for details about this release."
+
+# Find the latest tag
+# We could add this logic: minor or major. Then the system will manage the SEMVERSION automatically
+# tag_version="$(git tag --sort=-creatordate | head -n1)" && echo ${tag_version}

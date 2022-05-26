207b29058d529e9b3148329b12c8545bc4e2d010
Pascal Andy <pascalandy@users.noreply.github.com>
2022-05-25 21:46:32 -0400

    Update config for bashlava
    Signed-off-by: Pascal Andy <pascalandy@users.noreply.github.com>

---
+++ .bl_override.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+### Overriding defaults var in `.bl_override.sh`
+
+APP_NAME="ghostfire"
+GITHUB_USER="firepress-org"
+APP_VERSION="4.47.3"
+
+### function options
+CFG_USE_GPG_SIGNATURE="true"
+
+### branch names
+CFG_DEFAULT_BRANCH="master"
